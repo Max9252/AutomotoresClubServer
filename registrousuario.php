5e3e79c2570ff7202298ca116e9ff95ee5b73571
jhonny9550 <jhonny9550@hotmail.com>
2016-12-09 12:12:13 -0500

    Print_r de php eliminados

--- registrousuario.php
+++ registrousuario.php
@@ -1,43 +1,38 @@
 <?php
-header('Content-type: text/html; charset=UTF-8');
-header('Content-Type: application/json');
 
-include(
-    "config/conexion_bd.inc.php"
-);
+    header('Content-type: text/html; charset=UTF-8');
+    header('Content-Type: application/json');
 
-if($conn){
-    $params = explode(",",$argv[1]);
-    $correo= $params[0];
-    $contrasena= $params[1];
+    include(
+        "config/conexion_bd.inc.php"
+    );
 
-    $arrayDatosUsuario="INSERT INTO AC_USUARIO_VEHICULO (Id, Correo, Contrasena, Estado) 
-    VALUES (USUVEHICULO_ID.NEXTVAL, '$correo', '$contrasena', 0)";
+    if($conn){
+        $params = explode(",",$argv[1]);
+        $correo= $params[0];
+        $contrasena= $params[1];
 
-    print_r($arrayDatosUsuario);
+        $arrayDatosUsuario="INSERT INTO AC_USUARIO_VEHICULO (Id, Correo, Contrasena, Estado) 
+        VALUES (USUVEHICULO_ID.NEXTVAL, '$correo', '$contrasena', 0)";
 
-    $ingresoDatosUsuario= oci_parse($conn, $arrayDatosUsuario);
+        $ingresoDatosUsuario= oci_parse($conn, $arrayDatosUsuario);
 
-    print_r($ingresoDatosUsuario);
+        $comprobacion=oci_execute($ingresoDatosUsuario);
 
-    $comprobacion=oci_execute($ingresoDatosUsuario);
+        oci_close($conn);
 
-    oci_close($conn);
-
-    if($comprobacion){
-        $res = array('status' => true, 'message' => 'Success signUp');
+        if($comprobacion){
+            $res = array('status' => true, 'message' => 'Success signUp');
+            echo json_encode($res);
+        }
+        else{
+            $res = array('status' => false, 'message' => 'Error on signUp');
+            echo json_encode($res);
+        }
+        
+    }else{
+        $res = array('status' => false, 'message' => 'Connection error');
         echo json_encode($res);
     }
-    else{
-        $res = array('status' => false, 'message' => 'Error on signUp');
-        echo json_encode($res);
-    }
-    
-}else{
-    $res = array('status' => false, 'message' => 'Connection error');
-    echo json_encode($res);
-}
-
-
 
 ?>
