2d03072613729cdf00a8d7929d39bec96f6c24e6
jhonny9550 <jhonny9550@hotmail.com>
2016-12-15 10:09:01 -0500

    Bug de get en php corregido

--- php/getClaseVehiculo.php
+++ php/getClaseVehiculo.php
@@ -13,11 +13,22 @@ if($conn){
     oci_execute($resultado);
     $cerrar=oci_close($conn);
     $resultado = oci_fetch_array($resultado, OCI_ASSOC);
+    $resultado = utf8ize($resultado);
     $datos = array('status' => true, 'datos' => $resultado);
     echo json_encode($datos);
 }
 else{
     $resultado = array('status' => false, 'message' => 'Connection error');
     echo json_encode($resultado);
+}
+function utf8ize($d) {
+   if (is_array($d)) {
+       foreach ($d as $k => $v) {
+           $d[$k] = utf8ize($v);
+       }
+   } else if (is_string ($d)) {
+       return utf8_encode($d);
+   }
+   return $d;
 }
 ?>
