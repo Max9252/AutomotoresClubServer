22c841981ffb413b706142b94abe776bc0ac80b9
jhonny9550 <jhonny9550@hotmail.com>
2016-12-15 10:41:43 -0500

    Correccion de filas en los select

--- php/getClaseVehiculo.php
+++ php/getClaseVehiculo.php
@@ -3,32 +3,24 @@
 header('Content-type: text/html; charset=UTF-8');
 header('Content-Type: application/json');
 include(
-    "config/conexion_bd.inc.php"
+   "config/conexion_bd.inc.php"
 );
 if($conn){
-    //Query de insercion de registro
-    $query="SELECT CODIGO, NOMBRE FROM AC_P_CLASE_VEHICULO";
+   //Query de insercion de registro
+   $query="SELECT CODIGO, NOMBRE FROM AC_P_CLASE_VEHICULO";
 
-    $resultado= oci_parse($conn, $query);
-    oci_execute($resultado);
-    $cerrar=oci_close($conn);
-    $resultado = oci_fetch_array($resultado, OCI_ASSOC);
-    $resultado = utf8ize($resultado);
-    $datos = array('status' => true, 'datos' => $resultado);
-    echo json_encode($datos);
+   $resultado= oci_parse($conn, $query);
+   oci_execute($resultado);
+   $cerrar=oci_close($conn);
+   while($row = oci_fetch_assoc($resultado))
+   {
+       $rows[] = $row;
+   }
+   $datos = array('status' => true, 'datos' => $rows);
+   echo json_encode($datos);
 }
 else{
-    $resultado = array('status' => false, 'message' => 'Connection error');
-    echo json_encode($resultado);
-}
-function utf8ize($d) {
-   if (is_array($d)) {
-       foreach ($d as $k => $v) {
-           $d[$k] = utf8ize($v);
-       }
-   } else if (is_string ($d)) {
-       return utf8_encode($d);
-   }
-   return $d;
+   $resultado = array('status' => false, 'message' => 'Connection error');
+   echo json_encode($resultado);
 }
 ?>
