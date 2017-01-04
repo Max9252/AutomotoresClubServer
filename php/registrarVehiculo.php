a097ea2ed97c30f5d51a1c4995c3eaae8d0f894f
Max9252 <alejandro.garay94@gmail.com>
2017-01-04 10:11:19 -0500

    Registro vehiculo con imagen

--- php/registrarVehiculo.php
+++ php/registrarVehiculo.php
@@ -17,13 +17,14 @@ if($conn){
     $id_usuario=$params[7];
     $aseguradora=$params[8];
     $color_vehiculo=$params[9];
+    $url=$params[10];
     //Estado como activo por defecto
     $estado=0; 
 
     //echo array($placa,$fecha_vigencia,$tipo_vehiculo,$linea,$barrio,$convenio,$modelo,$id_usuario,$aseguradora,$color_vehiculo);
 
     //Query de insercion de registro
-    $query="INSERT INTO AC_VEHICULO(ID, PLACA, VIGENCIA_SOAT, TIPO_VEHICULO, LINEA, BARRIO, CONVENIO, MODELO, ESTADO, ID_USUARIO, ASEGURADORA, COLOR_VEHICULO) VALUES (VEHICULO_ID.NEXTVAL, '$placa', to_date('$fecha_vigencia','YYYY-MM-DD'), $tipo_vehiculo, $linea, $barrio, $convenio, $modelo, $estado, $id_usuario, $aseguradora, $color_vehiculo)";
+    $query="INSERT INTO AC_VEHICULO(ID, PLACA, VIGENCIA_SOAT, TIPO_VEHICULO, LINEA, BARRIO, CONVENIO, MODELO, ESTADO, ID_USUARIO, ASEGURADORA, COLOR_VEHICULO, URL) VALUES (VEHICULO_ID.NEXTVAL, '$placa', to_date('$fecha_vigencia','YYYY-MM-DD'), $tipo_vehiculo, $linea, $barrio, $convenio, $modelo, $estado, $id_usuario, $aseguradora, $color_vehiculo, $url)";
 
     $vehiculo= oci_parse($conn, $query);
 
