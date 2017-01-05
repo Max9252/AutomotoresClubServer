a396fdd1cb2fe5f991ce1481f6cadbf52fcd7dbc
Max9252 <alejandro.garay94@gmail.com>
2017-01-05 14:07:00 -0500

    Prueba validacion placa

--- php/validarPlaca.php
+++ php/validarPlaca.php
@@ -8,7 +8,7 @@ include(
 if($conn){
     $params = explode(",",$argv[1]);
     $placa=$params[0];
-    $disponible=0;
+    $disponible=0; //Codigo 0 = Disponible
     //Query de insercion de registro
     $query="SELECT PLACA FROM AC_VEHICULO WHERE PLACA = '${placa}'";
     $resultado= oci_parse($conn, $query);
@@ -16,9 +16,9 @@ if($conn){
     $cerrar=oci_close($conn);
     $row = oci_fetch_array($resultado, OCI_ASSOC);
     if ($row['PLACA']==$placa ){
-    $disponible = 1;
+    $disponible = 1; //Codigo 1 = No disponible
     }
-    $datos = array('status' => $disponible, 'data'=>$placa);
+    $datos = array('status' => $disponible, 'data'=>$row['PLACA']);
     echo json_encode($datos);
 }
 else{
