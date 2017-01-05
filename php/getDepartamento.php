2eb4075ebafe1b1cbc689973950457c8f668c1d6
Max9252 <alejandro.garay94@gmail.com>
2017-01-05 13:46:25 -0500

    Se reconfigura el perfil del vehiculo

--- php/getDepartamento.php
+++ php/getDepartamento.php
@@ -8,7 +8,6 @@ include(
 if($conn){
     //Query de insercion de registro
     $query="SELECT CODIGO, NOMBRE FROM AC_P_DEPARTAMENTO";
-
     $resultado= oci_parse($conn, $query);
     oci_execute($resultado);
     $cerrar=oci_close($conn);
