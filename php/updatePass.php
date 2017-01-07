66fbac65c3b98da9ef4ac11ee3a591d214e02261
Max9252 <alejandro.garay94@gmail.com>
2017-01-07 12:52:27 -0500

    se agrega cambio de contrasena

---
+++ php/updatePass.php
@@ -0,0 +1,44 @@
+<?php
+header('Content-type: text/html; charset=UTF-8');
+header('Content-Type: application/json');
+include(
+    "config/conexion_bd.inc.php"
+);
+if($conn){
+
+    $contrasena= "hola";
+    $id= 1;
+
+    //Query de insercion de registro
+    $query="UPDATE AC_USUARIO_VEHICULO SET CONTRASENA='${contrasena}' WHERE ID='${id}'";
+
+    $resultado= @oci_parse($conn, $query);
+
+    oci_execute($resultado);
+
+    if($resultado){
+        $query2 = "SELECT CONTRASENA FROM AC_USUARIO_ADMINISTRADOR WHERE ID='${id}'";
+
+        $contrasena= oci_parse($conn, $query2);
+
+        $resul2= @oci_execute($contrasena);
+
+        $aux= oci_fetch_array($contrasena, OCI_ASSOC);
+
+        if ($aux['CONTRASENA'] === $contrasena) {    
+            $res = array('status' => true, 'message' => 'Update complete');
+        }else{
+            $res = array('status' => false, 'message' => 'Update failed');
+        }
+
+    }else{
+        $res = array('status' => false, 'message' => 'Update failed');
+    }
+
+
+}
+else{
+    $res = array('status' => false, 'message' => 'Connection error');
+    echo json_encode($res);
+}
+?>
