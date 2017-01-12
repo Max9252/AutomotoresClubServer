7e689b9281ffbedc2721bca02bf9a011c4c5e99a
Max9252 <alejandro.garay94@gmail.com>
2017-01-12 18:15:37 -0500

    UPDATE URL IMAGEN VEHICULO

---
+++ php/updateImagenVehiculo.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-type: text/html; charset=UTF-8');
+header('Content-Type: application/json');
+include(
+    "config/conexion_bd.inc.php"
+);
+if($conn){
+
+
+    $params = explode(",",$argv[1]);
+
+    $id=$params[0];
+    $url= $params[1];
+    
+    //Query de insercion de registro
+    $cambioPass="UPDATE AC_VEHICULO SET URL_IMAGEN='${url}' WHERE ID = $id";
+
+    $resultado= oci_parse($conn, $cambioPass);
+
+    oci_execute($resultado);
+
+    if($resultado){
+
+        $res = array('status' => true, 'message' => 'Update complete');
+
+    }else{
+        $res = array('status' => false, 'message' => 'Update failed2');
+    }
+
+    echo json_encode($res);
+
+}
+else{
+    $res = array('status' => false, 'message' => 'Connection error');
+    echo json_encode($res);
+}
+?>
