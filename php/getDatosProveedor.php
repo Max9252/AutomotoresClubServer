a36614bde645afcb511677f3318fd12ec5ea10f9
Alejandro <Alejandro>
2017-03-16 18:44:09 -0500

    update

--- php/getDatosProveedor.php
+++ php/getDatosProveedor.php
@@ -6,11 +6,11 @@ include(
     "config/conexion_bd.inc.php"
 );
 if($conn){
-    $params = explode(",",$argv[1]); 
-    $idEstablecimiento=$params[0]; 
+    $params = explode(",",$argv[1]);
+    $idEstablecimiento=$params[0];
 
     //Query de insercion de registro
-    $query="SELECT A.NOMBRE_COMERCIAL, A.TELEFONO_FIJO, A.DIRECCION, A.DESCRIPCION, A.URL_IMAGEN, B.NOMBRE AS CIUDAD FROM AC_ESTABLECIMIENTO_COMERCIO A, AC_P_CIUDAD B WHERE  A.CIUDAD = B.CODIGO AND A.ID=$idEstablecimiento";
+    $query="SELECT A.NOMBRE_COMERCIAL, A.TELEFONO_FIJO, A.DIRECCION, A.DESCRIPCION, A.URL_IMAGEN, B.NOMBRE AS CIUDAD FROM AC_ESTABLECIMIENTO A, AC_P_CIUDAD B WHERE  A.CIUDAD = B.CODIGO AND A.ID=$idEstablecimiento";
 
     $resultado= oci_parse($conn, $query);
     oci_execute($resultado);
