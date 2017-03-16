e2904b7f84cf5deaff365695ff7c815fb512cbe1
Alejandro <Alejandro>
2017-03-16 17:37:48 -0500

    Update 3

--- config/conexion_bd.inc.php
+++ config/conexion_bd.inc.php
@@ -2,9 +2,5 @@
     require_once('bd-config.inc.php');
     //Conexion a la base de datos
     $conn = oci_connect(USERNAME, PASSWORD, TNS);
-    if($conn){
-      echo "hay conexion";
-    }else{
-      echo "no hay conexion";
-    }
+
 ?>
